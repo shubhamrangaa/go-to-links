628465630cd90d5337f02bd8ab610938ee88d686
shubham <shubhamranga@gmail.com>
2021-05-21 15:14:14 +0530

    new UI, completely functional

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
-// const Links = {
-//   facebook: "",
-//   instagram: "",
-//   twitter: "",
-//   linkedin: "",
-// };
-
 window.onload = function () {
+  const name = document.profileForm.name;
   const linkedin = document.profileForm.linkedin;
   const facebook = document.profileForm.facebook;
   const instagram = document.profileForm.instagram;
   const twitter = document.profileForm.twitter;
-  const form = document.profileForm;
+  const portfolio = document.profileForm.portfolio;
   const submit = document.getElementById("submitForm");
+
+  chrome.storage.sync.get(["linkedin"], function (result) {
+    linkedin.value = result.linkedin;
+  });
+  chrome.storage.sync.get(["name"], function (result) {
+    name.value = result.name;
+  });
+  chrome.storage.sync.get(["twitter"], function (result) {
+    twitter.value = result.twitter;
+  });
+  chrome.storage.sync.get(["instagram"], function (result) {
+    instagram.value = result.instagram;
+  });
+  chrome.storage.sync.get(["portfolio"], function (result) {
+    portfolio.value = result.portfolio;
+  });
+  chrome.storage.sync.get(["facebook"], function (result) {
+    facebook.value = result.facebook;
+  });
 
   submit.onclick = () => {
     console.log(linkedin.value);
-    chrome.storage.sync.set({ linkedin: linkedin.value }, function () {
-      console.log("Value is set to " + linkedin);
-    });
+    chrome.storage.sync.set(
+      {
+        name: name.value,
+        linkedin: linkedin.value,
+        twitter: twitter.value,
+        facebook: facebook.value,
+        instagram: instagram.value,
+        portfolio: portfolio.value,
+      },
+      function () {
+        console.log("Value is set to " + linkedin);
+      }
+    );
   };
 
   console.log(linkedin.value);
--- read.js
+++ read.js
@@ -1,18 +1,48 @@
 window.onload = function () {
-  // const linkedin = document.profileForm.linkedin;
-  // const facebook = document.profileForm.facebook;
-  // const instagram = document.profileForm.instagram;
-  // const twitter = document.profileForm.twitter;
-  // const form = document.profileForm;
-  let linkedinLink = document.getElementById("linkedinLink");
-  let linkedinButton = document.getElementById("linkedinLinkButton");
+  const profileName = document.getElementById("profileName");
+  let displayLink = document.getElementById("displayLink");
+  let linkedinButton = document.getElementById("linkedinButton");
+  let twitterButton = document.getElementById("twitterButton");
+  let portfolioButton = document.getElementById("portfolioButton");
+  let facebookButton = document.getElementById("facebookButton");
+  let instagramButton = document.getElementById("instagramButton");
 
+  chrome.storage.sync.get(["name"], function (result) {
+    profileName.innerHTML = `Hi ${result.name}, how are you?`;
+  });
   linkedinButton.onclick = () => {
     chrome.storage.sync.get(["linkedin"], function (result) {
-      linkedinLink.value = result.linkedin;
-      linkedinLink.select();
+      displayLink.value = result.linkedin;
+      displayLink.select();
       document.execCommand("copy");
-      console.log("Value currently is " + result.linkedin);
+    });
+  };
+  twitterButton.onclick = () => {
+    chrome.storage.sync.get(["twitter"], function (result) {
+      displayLink.value = result.twitter;
+      displayLink.select();
+      document.execCommand("copy");
+    });
+  };
+  portfolioButton.onclick = () => {
+    chrome.storage.sync.get(["portfolio"], function (result) {
+      displayLink.value = result.portfolio;
+      displayLink.select();
+      document.execCommand("copy");
+    });
+  };
+  facebookButton.onclick = () => {
+    chrome.storage.sync.get(["facebook"], function (result) {
+      displayLink.value = result.facebook;
+      displayLink.select();
+      document.execCommand("copy");
+    });
+  };
+  instagramButton.onclick = () => {
+    chrome.storage.sync.get(["instagram"], function (result) {
+      displayLink.value = result.instagram;
+      displayLink.select();
+      document.execCommand("copy");
     });
   };
 };
